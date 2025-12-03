a1b7738a9d71604f0ff2f1046665a8c0e797b3c2
Neha Kumarii 123 <nknehakumariii810@gmail.com>
2025-12-03 18:07:14 +0500

    Added setinterval and seTimeout

--- EventListeners/script.js
+++ EventListeners/script.js
@@ -1,6 +1,6 @@
-// const grandparent=document.querySelector(".grandparent");
-// const parent=document.querySelector(".parent");
-// const child=document.querySelector(".child");
+const grandparent=document.querySelector(".grandparent");
+const parent=document.querySelector(".parent");
+const child=document.querySelector(".child");
 
 // grandparent.addEventListener('click', e =>{
 //     console.log (e.target)
@@ -53,64 +53,64 @@
 
 //  Part 2
 
-// grandparent.addEventListener("click", e =>{
-//     console.log ("Grandparent Bubble")
-// })
+grandparent.addEventListener("click", e =>{
+    console.log ("Grandparent Bubble")
+})
 
-// parent.addEventListener("click", () =>{
+parent.addEventListener("click", () =>{
+    console.log("hi")
+
+})   
+     setTimeout(()=>{
+        parent.removeEventListener("click", () =>{
+            console.log("hi")
+        })
+
+     }, 30000)
+
+child.addEventListener("click", e =>  {
+   
+   
+    console.log ("Child Bubble")
+})
+
+function printHi(){
+    console.log("Hi")
+
+}
+
+
+// //Part 3
+
+// const divs=document.querySelectorAll('div')
+
+// // divs.forEach(div =>{
+// //     div.addEventListener('click',() =>{
+
+// //         console.log("hi")
+// //     })
+// // })
+
+// addGlobalEventListener("click", 'div', e =>{
+//    if(e.target.matches("div")){
 //     console.log("hi")
-
-// })   
-//      setTimeout(()=>{
-//         parent.removeEventListener("click", () =>{
-//             console.log("hi")
-//         })
-
-//      }, 30000)
-
-// child.addEventListener("click", e =>  {
-   
-   
-//     console.log ("Child Bubble")
+// }
 // })
 
-// function printHi(){
-//     console.log("Hi")
+// function addGlobalEventListener(type, selector, callback){
+
+//     document.addEventListener(type, e =>{
+//         if (e.target.matches(selector))callback(e)
+//     })
 
 // }
 
+// const newDiv =document.createElement('div')
+// newDiv.style.width='200px'
 
-//Part 3
-
-const divs=document.querySelectorAll('div')
-
-// divs.forEach(div =>{
-//     div.addEventListener('click',() =>{
-
-//         console.log("hi")
-//     })
-// })
-
-addGlobalEventListener("click", 'div', e =>{
-   if(e.target.matches("div")){
-    console.log("hi")
-}
-})
-
-function addGlobalEventListener(type, selector, callback){
-
-    document.addEventListener(type, e =>{
-        if (e.target.matches(selector))callback(e)
-    })
-
-}
-
-const newDiv =document.createElement('div')
-newDiv.style.width='200px'
-
-newDiv.style.height='200px'
-newDiv.style.backgroundColor="purple"
-// newDiv.addEventListener ('click', () =>{
-//     console.log("hi")
-// })
-document.body.append(newDiv)
+// newDiv.style.height='200px'
+// newDiv.style.backgroundColor="purple"
+// // newDiv.addEventListener ('click', () =>{
+// //     console.log("hi")
+// // })
+// document.body.append(newDiv)
