23fd04cfb25fe454fc19c6ad2bc95c7a36b32b9b
Neha Kumarii 123 <nknehakumariii810@gmail.com>
2025-12-05 20:22:09 +0500

    Added closures

---
+++ Closures/index.js
@@ -0,0 +1,22 @@
+function z(){
+
+    var b=900;
+    function x (){
+    var a=25;
+    function y(){
+        console.log(a, b);
+    }
+    y();
+    // a=190;
+    // return y;
+
+}
+x();
+}
+
+z();
+// var z=x();
+// console.log(z);
+
+// //.........
+// z();
